8b3fa888ed4a0a7ad12ca2ac3ecc5346f4336285
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-09-20 13:23:08 -0500

    ADD: adaptador para no depender de axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { PokemonService } from 'src/pokemon/pokemon.service';
 import { PokeResponse } from './interfaces/poke-response';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
-  constructor(private readonly pokemonService: PokemonService) {
+
+  constructor(
+    private readonly http: AxiosAdapter,
+    private readonly pokemonService: PokemonService) {
 
   }
   async executeSeed() {
     await this.pokemonService.removeMany();
-    
-    const { data } = await this.axios.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=650");
+
+    const data = await this.http.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=50");
 
     const dataFormated = data.results.map(({ name, url }) => {
       const segments = url.split("/");
