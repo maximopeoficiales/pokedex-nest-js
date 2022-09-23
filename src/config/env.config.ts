c907baa9fb939b85fd01593a346ff39d3e08a442
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-09-23 18:55:40 -0500

    change: readme.md variables de entorno

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,7 +3,7 @@ import { ConfigEnv } from "./interfaces/config.interface";
 export const EnvConfiguration = (): ConfigEnv => (
   {
     ENV: process.env.NODE_ENV || 'dev',
-    MONGO_DB: process.env.MONGO_BD,
+    MONGO_DB: process.env.MONGO_DB,
     PORT: +process.env.PORT || 3002,
     DEFAULT_LIMIT: +process.env.DEFAULT_LIMIT || 5,
   }
