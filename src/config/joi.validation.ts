a1d652279d81c8819fb4c6ae8105dbd60adf339f
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-09-24 18:06:13 -0500

    change validator

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -4,6 +4,6 @@ import { ConfigEnv } from "./interfaces/config.interface";
 export const JoiValidationSchema = Joi.object<ConfigEnv>({
   MONGO_DB: Joi.required(),
   PORT: Joi.number().default(3000),
-  DEFAULT_LIMIT: Joi.number().default(5),
+  DEFAULT_LIMIT: Joi.number().default(10),
   ENV: Joi.string().default("dev"),
 });
